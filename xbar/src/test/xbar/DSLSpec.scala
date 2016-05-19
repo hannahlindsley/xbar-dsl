e5333b9fe2421f48346082b9bccec611ccf154b6
hannahlindsley <hannah@cognitivescale.com>
2016-05-19 10:24:05 -0500

    Converted spec to compilation tests using matchers

--- xbar/src/test/xbar/DSLSpec.scala
+++ xbar/src/test/xbar/DSLSpec.scala
@@ -1,51 +1,81 @@
 package xbar
 
-import xbar.projection._
-
+import projection._
 import org.scalatest.{FunSpec, Matchers}
 
 /**
- * There is no way to "run" this test suite; compilation of the code
- * (and compilation errors on the commented out code) signifies that
- * the tests have passed.
+ * Note that the import of the projection directory is necessary for these tests to be run.
  */
 class DSLSpec extends FunSpec with Matchers {
 
   describe("A verb phrase") {
 
     it ("should be complementable by DP and JP") {
-      VP(V_(V("eat"),  DP(D_(D("the"), NP(N_(N("mouse")))))))
-      VP(V_(V("be"), JP(J_(J("happy")))))
+      "VP(V_(V(\"eat\"), DP(D_(D(\"the\"), NP(N_(N(\"mouse\")))))))" should compile
+      "VP(V_(V(\"be\"), JP(J_(J(\"happy\")))))" should compile
     }
   }
 
   describe ("A determiner phrase") {
 
     it ("can be complemented by an NP") {
-      DP(D_(D("the"), NP(N_(N("tree")))))
+      "DP(D_(D(\"the\"), NP(N_(N(\"tree\")))))" should compile
     }
 
     it ("should not (ever) be uncomplemented") {
-//      DP(D_(D("the"))))
+      "DP(D_(D(\"the\"))))" shouldNot compile
     }
 
     it ("should be specifiable only by a determiner phrase") {
-      DP(DP(D_(D(""), NP(N_(N("Hannah"))))), D_(D("'s"), NP(N_(N("cat")))))
+      "DP(DP(D_(D(\"\"), NP(N_(N(\"Hannah\"))))), D_(D(\"'s\"), NP(N_(N(\"cat\")))))" should compile
     }
   }
 
-  describe ("a noun phrase") {}
+  describe ("a noun phrase") {
 
-  describe ("an adjective phrase") {}
+    it ("can be complemented by a PP") {
+
+    }
+
+    it ("can be specified by nothing") {
+
+    }
+
+    it ("can be adjoined by a UP") {
+
+    }
+  }
+
+  describe ("an adjective phrase") {
+
+  }
 
   describe ("an adverb phrase") {}
 
-  describe ("a prepositional phrase") {}
+  describe ("a prepositional phrase") {
 
-  describe ("a union phrase") {}
+    it ("must be complemented") {
 
-  describe ("a complementizer phrase") {}
+    }
 
-  describe ("a tense phrase") {}
+    it ("can be complemented by DP") {
+
+    }
+  }
+
+  describe ("a union phrase") {
+
+  }
+
+  describe ("a complementizer phrase") {
+
+  }
+
+  describe ("a tense phrase") {
+
+    it ("must be complemented") {}
+
+    it ("can only be complemented by a verb phrase") {}
+  }
 
 }
